5347a67fe14aaafb7cf729719d2a697e8df529bd
VasekZah <vaclavzahradka26@gmail.com>
2025-08-12 16:10:03 +0200

    Create index.js

---
+++ js/classes/index.js
@@ -0,0 +1,5 @@
+export { Settler } from './settler.js';
+export { Building } from './building.js';
+export { WorldObject } from './worldObject.js';
+export { Animal } from './animal.js';
+export { Projectile } from './projectile.js';
